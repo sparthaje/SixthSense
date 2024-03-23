b08c8ccf6acc042d61fab73e2971d42f9191ab79
Shreepa Parthaje <shreepa.parthaje@gmail.com>
2024-03-23 12:52:09 -0400

    Initial Commit

---
+++ SixthSense/SixthSenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SixthSenseApp.swift
+//  SixthSense
+//
+//  Created by Shreepa Parthaje on 3/23/24.
+//
+
+import SwiftUI
+
+@main
+struct SixthSenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
